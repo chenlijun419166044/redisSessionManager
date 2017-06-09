77614fd5b303a94eb3ba27d9645fe61c98554b2e
chenlijun <419166044@qq.com>
2017-06-09 22:45:07 +0800

    [redis][opt]:新增

---
+++ web/src/main/java/com/pp/web/Demo.java
@@ -0,0 +1,19 @@
+package com.pp.web;
+
+import com.pp.common.AmountUtils;
+
+/**
+ * Created by chenlijun on 2017/6/9.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        try {
+            AmountUtils.changeF2Y(2222L);
+        }catch (Exception e){
+
+        }finally {
+
+        }
+
+    }
+}
